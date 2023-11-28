ea55937a42249d068f0740b8ac59089b6df3d0a9
Soren Rajani <SorenRajani@gmail.com>
2023-11-27 18:18:15 -0800

    trained large model

--- train.py
+++ train.py
@@ -6,10 +6,10 @@ import pickle
 if __name__ == "__main__":
     time1 = time.time()
     trainer = poker_bot()
-    trainer.train(n_iter=10**8)
+    trainer.train(n_iter=10**6)
     print(abs(time1 - time.time()))
     print(sys.getsizeof(trainer))
 
     # Saving the model
-    with open("save.p", "wb") as f:
+    with open("bot.p", "wb") as f:
         pickle.dump(trainer, f)
